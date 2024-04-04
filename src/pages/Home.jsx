9732dd84cb47cb0849e85a6decadca765e4638b0
Mar Carrera <kmcarreraprieto@gmail.com>
2024-04-03 21:34:40 -0600

    construyecto vista Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="home-page">
+            <header className="h-100 min-vh-100 d-flex align-items-center text-light shadow">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6 d-flex d-sm-block flex-column align-items-center">
+                            <h2 className="mb-0 text-black fw-bold">Bienvenido a</h2>
+                            <h1 className="mb-5 text-black fw-bold text-center text-sm-start">React Restaurant</h1>
+                            {/* MIN 27:26 https://www.youtube.com/watch?v=MczKE_IWNjQ */}
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </header>
+            
+        </div>
+    )
+}
+
+export default Home;
